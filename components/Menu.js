e934bf09ee1143bfd553552b8f2be1be58908579
Marcio Iwamoto <kendi_marcio@hotmail.com>
2021-11-11 20:25:29 +1300

    tasks

--- components/Menu.js
+++ components/Menu.js
@@ -4,12 +4,10 @@ import styles from '../styles/Home.module.scss'
 
 import TaskSection1 from '../tasks/TaskSection1'
 import TaskSection2 from '../tasks/TaskSection2'
-import TaskSection3 from '../tasks/TaskSection3'
 
 const tasks = {
     task1: TaskSection1,
     task2: TaskSection2,
-    task3: TaskSection3,
 }
 
 const Menu = () => {
@@ -17,11 +15,10 @@ const Menu = () => {
     const Gettask = tasks[exercise];
 
     return (
-        <div>
-            <div id="menu">
-            <button onClick={() => { setExercise('task1') }} >Task 1</button>
-            <button onClick={() => { setExercise('task2') }} >Task 2</button>
-            <button onClick={() => { setExercise('task3') }} >Task 3</button>
+        <div className={['row col-md-4 col-12']}>
+            <div id="menu" className={styles.menu}>
+            <button className={styles.button} onClick={() => { setExercise('task1') }} >Task 1</button>
+            <button className={styles.button} onClick={() => { setExercise('task2') }} >Task 2</button>
             </div>
             <Modal showModal={Boolean(Gettask)}>
                 <Gettask />
